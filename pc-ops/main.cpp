8419f829d520845976ec2a190718b6a070df59b0
Darckoune <darckoune@gmail.com>
2018-11-26 08:27:32 +0100

    Removed useless messages on websocket

--- pc-ops/main.cpp
+++ pc-ops/main.cpp
@@ -90,21 +90,13 @@ int main() {
 
     cout << "Server: Message received: \"" << message_str << "\" from " << connection.get() << endl;
 
-    cout << "Server: Sending message \"" << message_str << "\" to " << connection.get() << endl;
-
-    auto send_stream = make_shared<WsServer::SendStream>();
-    *send_stream << message_str;
-    // connection->send is an asynchronous function
-    connection->send(send_stream, [](const SimpleWeb::error_code &ec) {
-      if(ec) {
-        cout << "Server: Error sending message. " <<
-            // See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for error code meanings
-            "Error: " << ec << ", error message: " << ec.message() << endl;
-      }
-    });
   };
 
   std::thread t{listenToBluetooth};
+
+  //////////////////////////////////////////////
+  //////////////// MAIN LOOP ///////////////////
+  //////////////////////////////////////////////
 
   while (1) {
     read(fd, &ev, sizeof(struct input_event));
